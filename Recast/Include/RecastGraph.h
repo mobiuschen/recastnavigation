ca674f04fa7920861f97f1321b3a9f8c5a300fa9
Mobius <mobius.chen@gmail.com>
2017-12-27 22:50:44 +0800

    Use edgeMatrix to improve the data structure of rcGraph.

--- Recast/Include/RecastGraph.h
+++ Recast/Include/RecastGraph.h
@@ -7,7 +7,7 @@ struct rcGraph
     unsigned short poly;        ///< The poly which this graph represents. It's valid only on level 0.
     rcGraph* verts;             ///< The graph vertices. [Length: #nverts]
     unsigned short* weights;    ///< The weight of vertices. [Length: #nverts]
-    unsigned short* edges;      ///< The graph edges. [Length: #nedges * 2]
+    unsigned short* edgeMatrix; ///< The graph edges. [Length: #nedges * #nedges]. The value of every edge is the weight.
     int nverts;                 ///< The number of graph vertices.
     int nedges;                 ///< The number of edges
 };
--- Recast/Source/RecastGraph.cpp
+++ Recast/Source/RecastGraph.cpp
@@ -13,10 +13,9 @@ rcGraph* rcAllocGraph()
 bool rcBuildGraph(rcContext* ctx, const rcPolyMesh& pmesh, rcGraph& graph)
 {
     unsigned int length = 0;
-    int maxEdages = pmesh.npolys == 0 ? 0 : (pmesh.npolys - 1) * 2;
-    int nedges = 0;
-
-    graph.nverts = pmesh.npolys;
+    const int npolys = pmesh.npolys;
+    const int nvp = pmesh.nvp;
+    graph.nverts = npolys;
 
 
     length = graph.nverts;
@@ -36,73 +35,39 @@ bool rcBuildGraph(rcContext* ctx, const rcPolyMesh& pmesh, rcGraph& graph)
     }
     memset(graph.weights, 1, sizeof(unsigned short) * length);
 
-    length = maxEdages * 2;
-    graph.edges = (unsigned short*)rcAlloc(sizeof(unsigned short) * length, RC_ALLOC_PERM);
-    if (!graph.edges)
+    length = npolys * npolys;
+    graph.edgeMatrix = (unsigned short*)rcAlloc(sizeof(unsigned short) * length, RC_ALLOC_PERM);
+    if (!graph.edgeMatrix)
     {
-        ctx->log(RC_LOG_ERROR, "rcBuildGraph: Out of memory 'graph.edges' (%d).", length);
+        ctx->log(RC_LOG_ERROR, "rcBuildGraph: Out of memory 'graph.edgeMatrix' (%d).", length);
         return false;
     }
+    memset(graph.edgeMatrix, 0, sizeof(unsigned short) * length);
 
-    length = maxEdages * 2;
-    rcScopedDelete<unsigned short> edges((unsigned short*)rcAlloc(sizeof(unsigned short) * length, RC_ALLOC_TEMP));
-    if (!edges)
-    {
-        ctx->log(RC_LOG_ERROR, "rcBuildGraph: Out of memory 'edges' (%d).", length);
-        return false;
-    }
-    memset(edges, RC_MESH_NULL_IDX, sizeof(unsigned short) * length);
-
-    const int nvp = pmesh.nvp;
     //In Level 0, every graph represents a poly.
-    for (int i = 0, n = pmesh.npolys; i < n; i++)
+    for (int i = 0; i < npolys; i++)
     {
         unsigned short* p = &(pmesh.polys[i * nvp * 2]);
-        rcGraph graphVert = graph.verts[i];
-        // set vertex
+        rcGraph& graphVert = graph.verts[i];
+        // set vertex graph
         graphVert.verts = nullptr;
         graphVert.weights = nullptr;
-        graphVert.edges = nullptr;
+        graphVert.edgeMatrix = nullptr;
         graphVert.nverts = 0;
         graphVert.poly = (unsigned short)i;
 
+        unsigned short* adjs = &(p[nvp]);
         // build edges
         for (int j = 0, m = nvp; j < m; j++)
         {
-            unsigned short* pAdj = &(p[nvp]);
+            int pAdj = adjs[j];
             if (p[j] == RC_MESH_NULL_IDX) break;
-            if (pAdj[j] & 0x8000) continue;
+            if (pAdj & 0x8000) continue;
 
-            int bigP = i > pAdj[j] ? pAdj[j] : i;
-            int smallP = i > pAdj[j] ? i : pAdj[j];
-
-            bool findEdge = false;
-            for (int k = 0; k < nedges; k++)
-            {
-                unsigned short* edge = &(edges[k * 2]);
-                if (edge[0] == RC_MESH_NULL_IDX || edge[1] == RC_MESH_NULL_IDX)
-                    break;
-
-                if (edge[0] == smallP && edge[1] == bigP)
-                {
-                    findEdge = true;
-                    break;
-                }
-            }//for
-
-            if (!findEdge)
-            {
-                edges[nedges] = (unsigned short)smallP;
-                edges[nedges + 1] = (unsigned short)bigP;
-                nedges++;
-            }
+            graph.edgeMatrix[i * npolys + pAdj] = 1;
+            graph.edgeMatrix[pAdj * npolys + i] = 1;
         }//for
     }//for
-
-    length = nedges * 2;
-    graph.edges = (unsigned short*)rcAlloc(sizeof(unsigned short) * length, RC_ALLOC_PERM);
-    graph.nedges = nedges;
-    memcpy(graph.edges, edges, sizeof(unsigned short) * length);
 
     return true;
 }
@@ -113,7 +78,7 @@ void rcFreeGraph(rcGraph* pGraph)
     if (pGraph == nullptr)
         return;
 
-    rcFree(pGraph->edges);
+    rcFree(pGraph->edgeMatrix);
     rcFree(pGraph->verts);
     rcFree(pGraph->weights);
     rcFree(pGraph);
