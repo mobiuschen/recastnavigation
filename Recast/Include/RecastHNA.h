cfd0291659ce6e372388244e16aebe70110ee8e5
mobiuschen <mobius.chen@gmail.com>
2018-02-13 12:32:30 +0800

    Complete the rcBuildGraph() function.

--- Recast/Include/RecastHNA.h
+++ Recast/Include/RecastHNA.h
@@ -8,31 +8,34 @@ typedef unsigned short GraphID;
 typedef unsigned short Weight;
 
 class rcContext;
+
 struct rcPolyMesh;
 struct rcGraph;
 
 static const unsigned short MAX_POLY_NUM = 5120;
 static const int RC_INVALID_INDEX = -1;
+static const unsigned int RC_INVALID_VERTEX = 0xFFFF;
 
 struct rcVertex
 {
-    int             ipoly;  ///The index of poly in rcPolyMesh.
-    Weight          vwgt;   ///The weight of vertex
-    unsigned short  nedges; ///The size of the adjacency list of v
-    int             iedges; ///The index into Adjncy that is the beginning of the adjacency list of v
-    Weight          cewgt;  ///the weight of the edges that have been contracted to create vertex (if vertexis a multinode)
-    Weight          adjwgt; ///the sum of the weight of the edges adjacent to v
+    int ipoly;  ///The index of poly in rcPolyMesh.
+    Weight vwgt;   ///The weight of vertex
+    unsigned short nedges; ///The size of the adjacency list of v
+    int iedges; ///The index into Adjncy that is the beginning of the adjacency list of v
+    Weight cewgt;  ///the weight of the edges that have been contracted to create vertex (if vertexis a multinode)
+    Weight adjwgt; ///the sum of the weight of the edges adjacent to v
 };
 
 struct rcGraph
 {
-    rcVertex*   vtxs;
-    Weight*     adjncy; ///the adjacency lists of the vertices
-    int         nvt;    ///The number of vertices
+    rcVertex *vtxs;
+    Weight *adjncy; ///the adjacency lists of the vertices
+    int nvt;    ///The number of vertices
 };
 
 
-rcGraph*    rcAllocGraph();
-bool rcBuildGraph(rcContext* ctx, const rcPolyMesh& pmesh, rcGraph& graph);
+rcGraph *rcAllocGraph();
+
+bool rcBuildGraph(rcContext *ctx, const rcPolyMesh &pmesh, rcGraph &graph);
 
 #endif
--- Recast/Source/RecastHNA.cpp
+++ Recast/Source/RecastHNA.cpp
@@ -1,72 +1,91 @@
 #include <string.h>
-#include <stdlib.h>
 #include "Recast.h"
 #include "RecastHNA.h"
 
 
-rcGraph* rcAllocGraph()
+rcGraph *rcAllocGraph()
 {
-    rcGraph* graph = (rcGraph*)rcAlloc(sizeof(rcGraph), RC_ALLOC_PERM);
+    rcGraph *graph = (rcGraph *) rcAlloc(sizeof(rcGraph), RC_ALLOC_PERM);
     memset(graph, 0, sizeof(rcGraph));
     return graph;
 }
 
-bool rcBuildGraph(rcContext* ctx, const rcPolyMesh& pmesh, rcGraph& graph)
+bool rcBuildGraph(rcContext *ctx, const rcPolyMesh &pmesh, rcGraph &graph)
 {
     bool result = false;
     const int npolys = pmesh.npolys;
+    int nvts = 0;
     int allocLen = 0;
 
-    rcVertex* vtxs = nullptr;
-    Weight* adjlist = nullptr;
-
+    rcVertex *vtxs = nullptr;
+    Weight *adjMatrix = nullptr;
 
     allocLen = npolys;
-    vtxs = (rcVertex*)rcAlloc(sizeof(rcVertex) * allocLen, RC_ALLOC_PERM);
+    vtxs = (rcVertex *) rcAlloc(sizeof(rcVertex) * allocLen, RC_ALLOC_PERM);
     if (vtxs == nullptr)
     {
         ctx->log(RC_LOG_ERROR, "rcBuildGraph: Out of memory 'vtxs' (%d)", allocLen);
         goto Exit0;
     }
-    memset(vtxs, 0, sizeof(rcVertex) * allocLen);
+    memset(vtxs, RC_INVALID_VERTEX, sizeof(rcVertex) * allocLen);
 
     allocLen = npolys * npolys;
-    adjlist = (Weight*)rcAlloc(sizeof(Weight) * allocLen, RC_ALLOC_PERM);
-    if (adjlist == nullptr)
+    adjMatrix = (Weight *) rcAlloc(sizeof(Weight) * allocLen, RC_ALLOC_PERM);
+    if (adjMatrix == nullptr)
     {
-        ctx->log(RC_LOG_ERROR, "rcBuildGraph: Out of memory 'adjlist' (%d)", allocLen);
+        ctx->log(RC_LOG_ERROR, "rcBuildGraph: Out of memory 'adjMatrix' (%d)", allocLen);
         goto Exit0;
     }
-    memset(adjlist, 0, sizeof(Weight) * allocLen);
+    memset(adjMatrix, 0, sizeof(Weight) * allocLen);
 
 
     for (int i = 0; i < npolys; i++)
     {
-        unsigned short* poly = pmesh.polys + i * pmesh.nvp * 2;
-        unsigned short* padj = poly + pmesh.nvp;
-        for (int j = 0; j < npolys; j++)
+        if (pmesh.polys[i] == RC_MESH_NULL_IDX)
+            break;
+
+        unsigned short *poly = pmesh.polys + i * pmesh.nvp * 2;
+        unsigned short *padj = poly + pmesh.nvp;
+        for (int j = 0; j < pmesh.nvp; j++)
         {
-            unsigned short d = padj[j];
+            unsigned short ipoly = padj[j];
+            if (ipoly == RC_MESH_NULL_IDX)
+                continue;
 
-            if (d == RC_MESH_NULL_IDX)
-                break;
-
-
+            if (adjMatrix[i * npolys + ipoly] == 0)
+            {
+                adjMatrix[i * npolys + ipoly] = 1;
+                adjMatrix[ipoly * npolys + i] = 1;
+            }
         }
 
-        rcVertex& v = vtxs[i];
+        rcVertex &v = vtxs[i];
         v.ipoly = i;
         v.vwgt = 1;
-        v.nedges = 0; //TODO
-        v.iedges = RC_INVALID_INDEX;
+        v.nedges = 0; // set in next step
+        v.iedges = i * npolys;
         v.cewgt = 0;
-        v.adjwgt = 0;
+        v.adjwgt = 0; // set in next step
+        nvts++;
     }
+
+    for (int i = 0; i < nvts; i++)
+    {
+        rcVertex &v = vtxs[i];
+        for (int j = 0; j < nvts; j++)
+        {
+            if (adjMatrix[i * npolys + j] == 0)
+            {
+                v.adjwgt += adjMatrix[i * npolys + j];
+                v.nedges++;
+            }
+        }//for
+    }//for
 
 
     graph.vtxs = vtxs;
-    graph.adjncy = adjlist;
-    graph.nvt = npolys;
+    graph.adjncy = adjMatrix;
+    graph.nvt = nvts;
 
     result = true;
 Exit0:
@@ -78,10 +97,10 @@ Exit0:
             vtxs = nullptr;
         }
 
-        if (adjlist != nullptr)
+        if (adjMatrix != nullptr)
         {
-            rcFree(adjlist);
-            adjlist = nullptr;
+            rcFree(adjMatrix);
+            adjMatrix = nullptr;
         }
     }
     return result;
